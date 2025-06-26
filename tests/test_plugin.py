a9610c63c91fd3cde2c7b3841811d919cad1c823
angusmcb <git@zog.mozmail.com>
2025-06-26 17:33:20 +0200

    tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,22 +1,46 @@
-"""Tests for the Plugin class."""
+from qgis.core import QgsProject
 
-import sys
-from unittest.mock import Mock, call, patch
-
-import pytest
-from qgis.gui import QgsLayerTreeView
-
-from attributeshortcut.plugin import Plugin
+from attributeshortcut.plugin import AttributeShortcutIndicator, Plugin
 
 
-class TestPlugin:
-    """Test cases for the Plugin class."""
+def test_plugin_initialization():
+    Plugin()
 
-    def test_plugin_initialization(self):
-        """Test plugin initialization."""
 
-        Plugin()
+def test_plugin_init_gui():
+    plugin = Plugin()
+    plugin.initGui()
 
-    def test_plugin_init_gui(self):
-        plugin = Plugin()
-        plugin.initGui()
+
+def test_add_indicator_to_layers(qgis_countries_layer, qgis_iface, qgis_new_project):
+    QgsProject.instance().addMapLayer(qgis_countries_layer)
+
+    plugin = Plugin()
+    plugin.initGui()
+
+    layer_tree_layer = QgsProject.instance().layerTreeRoot().findLayer(qgis_countries_layer.id())
+    layer_tree_view = qgis_iface.layerTreeView()
+    assert (
+        sum(
+            isinstance(ind, AttributeShortcutIndicator)
+            for ind in layer_tree_view.indicators(layer_tree_layer)
+        )
+        == 1
+    )
+
+
+def test_add_indicator_to_layer_added_later(qgis_countries_layer, qgis_iface, qgis_new_project):
+    plugin = Plugin()
+    plugin.initGui()
+
+    QgsProject.instance().addMapLayer(qgis_countries_layer)
+
+    layer_tree_layer = QgsProject.instance().layerTreeRoot().findLayer(qgis_countries_layer.id())
+    layer_tree_view = qgis_iface.layerTreeView()
+    assert (
+        sum(
+            isinstance(ind, AttributeShortcutIndicator)
+            for ind in layer_tree_view.indicators(layer_tree_layer)
+        )
+        == 1
+    )
